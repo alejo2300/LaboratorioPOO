cae063fb9574f0fa983c7f0072f18ae83e101f5e
Fabio Alejandro Camargo Díaz <fcamargodiaz@gmail.com>
2019-02-09 21:02:03 -0500

    Main
    Pago de nomina y aporte al SENA corregidos

--- Empresa/src/empresa/Empresa.java
+++ Empresa/src/empresa/Empresa.java
@@ -5,6 +5,7 @@
  */
 package empresa;
 import Entidades.Empleado;
+import java.util.Scanner;
 /**
  *
  * @author alejo
@@ -15,7 +16,51 @@ public class Empresa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("SISTEMA DE NOMINAS||LABORATORIO POO\n\n");
+        
+        //Creacion de empleados
+        Empleado emp[]= new Empleado[10];
+        int n;
+        System.out.print("Digite el numero de empleados: ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        System.out.println("");
+        for(int i=0;i<n;i++){
+            System.out.println("");
+            String name;
+            System.out.print("Digite el nombre de su "+(i+1)+" trabajador: ");
+            name = sc.next();
+            long cedula;
+            System.out.print("Digite la cedula de su "+(i+1)+" trabajador: ");
+            cedula = sc.nextLong();
+            int salario;
+            System.out.print("Digite el salario de su "+(i+1)+" trabajador: ");
+            salario = sc.nextInt();
+            emp[i] = crearEmpleado(name,cedula,salario);
+        }
+        System.out.println("");
+        System.out.println("La nomina total de la empresa es "+NominaTotal(emp,n));
+        System.out.println("El aporte total de la empresa al SENA es "+NominaTotal(emp,n)*0.04);
     }
     
+    //Metodos externos
+    
+    public static Empleado crearEmpleado(String nombre,long cedula, double salario){
+       Empleado empleado=new Empleado(nombre,cedula,salario);
+       return empleado;
+    }
+    
+    public static void Informacion(Empleado empleado){
+        System.out.println("El nombre del empleado es: "+ empleado.getNombre()+" \nLa cedula del empleado es: "+empleado.getCedula()+"\nEl total devengado es: "+ empleado.TotalDevengado() + "\nEl total de descuentos es: "+ empleado.CalcularDescuentoTotal()+ "\nEl total de la nomina es: "+ empleado.CalcularTotalNomina()+ "\n");
+    }
+
+    public static double NominaTotal(Empleado Emp[],int j){
+        double k=0;
+        for(int q=0;q<j;q++){
+            k+=Emp[q].CalcularTotalNomina();
+        }
+           return k;
+    }      
 }
+
+
